ababf9d95477da386c18028c8a5c9941920451fc
4MN35T <tanmaykarmore01@gmail.com>
2023-04-05 09:38:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -34,12 +34,3 @@ class App extends Component {
 
 export default App;
 export const LocationDisplay = LocationDisplayComponent;
-
-
-// import React from "react";
-// import { useLocation } from "react-router-dom";
-
-// export default function LocationDisplayComponent() {
-//     let location = useLocation();
-//     return <div data-testid="location-display">{location.pathname}</div>;
-// }
